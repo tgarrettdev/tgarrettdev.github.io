992ba74b63ff8197f44036ea0b330c2178dea0ee
tgarrettdev <121262502+tgarrettdev@users.noreply.github.com>
2023-01-18 16:49:24 +0000

    form update

---
+++ scripts.js
@@ -0,0 +1,84 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+$(document).ready(function() {
+    // handle form submission
+    $('#contact-form').submit(function(event) {
+        event.preventDefault();
+        var formData = {
+            'name': $('input[name=name]').val(),
+            'email': $('input[name=email]').val(),
+            'phone': $('input[name=phone]').val(),
+            'message': $('textarea[name=message]').val()
+        };
+        // perform validation
+        if (!formData.name) {
+            alert("Name is required");
+            return;
+        }
+        if (!formData.email) {
+            alert("Email is required");
+            return;
+        }
+        if (!formData.phone) {
+            alert("Phone is required");
+            return;
+        }
+        if (!formData.message) {
+            alert("Message is required");
+            return;
+        }
+        if (!validateEmail(formData.email)) {
+            alert("Invalid Email");
+            return;
+        }
+
+        // process the form
+        $.ajax({
+            type: 'POST',
+            url: 'submit-form.php',
+            data: formData,
+            dataType: 'json',
+            encode: true
+        }).done(function(data) {
+            // handle successful submission
+            $('#submitButton').attr("disabled", true);
+            if (data.status === "success") {
+                $('#submitSuccessMessage').removeClass('d-none');
+            } else {
+                $('#submitErrorMessage').removeClass('d-none');
+            }
+        }).fail(function(data) {
+            // handle error
+            $('#submitErrorMessage').removeClass('d-none');
+        });
+    });
+
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+});
---
+++ server-side.php
@@ -0,0 +1,42 @@
+<?php
+    // check if the form has been submitted
+    if(isset($_POST) && !empty($_POST)){
+        // get form data
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $message = $_POST['message'];
+        
+        // basic validation
+        if(empty($name) || empty($email) || empty($phone) || empty($message)){
+            // return error message if any fields are empty
+            echo json_encode(array('status' => 'error', 'message' => 'Please fill in all fields'));
+            exit;
+        }
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            // return error message if email is invalid
+            echo json_encode(array('status' => 'error', 'message' => 'Invalid email'));
+            exit;
+        }
+        
+        // Send email
+        $to = 'aifinancialsolutions.com@gmail.com';
+        $subject = 'New contact form submission';
+        $headers = "From: " . strip_tags($email) . "\r\n";
+        $headers .= "Reply-To: ". strip_tags($email) . "\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+        $message = '<p>Name: '.$name.'</p>'
+                  .'<p>Email: '.$email.'</p>'
+                  .'<p>Phone: '.$phone.'</p>'
+                  .'<p>Message: '.$message.'</p>';
+        if(mail($to, $subject, $message, $headers)){
+            echo json_encode(array('status' => 'success', 'message' => 'Thank you for your message'));
+            exit;
+        }
+        else{
+            echo json_encode(array('status' => 'error', 'message' => 'Error sending message'));
+            exit;
+        }
+    }
+?>
